55c7f4d1ad136f084964f60208c28db6ebd9b968
Raihan Tanvir <raihanntanvir@gmail.com>
2025-02-26 17:13:07 -0600

    fixed instant closing, made bot continuous

--- brian.py
+++ brian.py
@@ -1,41 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import sys
 
 driver = webdriver.Chrome()
-
 url = "https://server.thecoderschool.com/thegames/results.php?game_id=B2F502ESNAKET&results_code_id=1&showall"
 counter = 0
-
-print("im about to brian everywhere")
-login = False
+login_flag = False
+print("im looking :o")
 
 while True:
     driver.get(url)
-    time.sleep(0.2)
+    time.sleep(0.1)
 
-    if login == False:
-        time.sleep(7)
-        login = True
+    if login_flag == False:
+        time.sleep(6)
+        login_flag = True
     
     try:
         unscored_links = driver.find_elements(By.XPATH, "//div[contains(text(),'Unscored Games')]/following-sibling::a")
-        
         if unscored_links:
             for link in unscored_links:
                 link_text = link.text.lower()
                 if "currently being scored by" not in link_text:
                     href = link.get_attribute("href")
                     if href and "score.php?game_id=" in href and "team_id=" in href:
+                        print("I found! :D")
                         print(f"unscored game link: {href}")
-                        link.click()
+                        driver.execute_script("window.open(arguments[0], '_blank');", href)
                         counter += 1
-                        print("i brianed")
                         with open("graded.txt", "w") as file:
                                 file.write(str(counter))
                         time.sleep(1)
-
+                        input("keep looking? :]")
+                        driver.switch_to.window(driver.window_handles[0])
+                        print("back to looking :o")
+                        break
+                    
     except Exception as e:
-        print("Error while processing unscored games:", e)
+        print("brokey :( ->", e)
 driver.quit()
     
